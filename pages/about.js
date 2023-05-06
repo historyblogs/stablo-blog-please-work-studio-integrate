ace904096539283851e7b9031e9e0eb2cb95d288
historyblogs <35374507+historyblogs@users.noreply.github.com>
2023-05-05 18:57:55 -0700

    Updated about page

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function About({ authors, siteconfig }) {
     <Layout {...siteconfig}>
       <Container>
         <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
-          About Nathan Marsak
+          About Nathan Marsak 
         </h1>
         <div className="text-center">
           <p className="text-lg">Blogging Los Angeles One Demolition Permit At A Time.</p>
